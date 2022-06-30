c04fdf1ab294d94ef13d8996b67de62c873e631d
Katherinepaucara <katherinepaucara26@gmail.com>
2022-06-30 20:07:06 -0300

    cambios header1
    logo arg-pro

--- PORTAFOLIO-PAUCARA.KATHERINE/src/app/app.module.ts
+++ PORTAFOLIO-PAUCARA.KATHERINE/src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoApComponent } from './components/logo-ap/logo-ap.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoApComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
